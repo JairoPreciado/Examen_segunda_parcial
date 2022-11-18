0049eaf7a6305697ca797c4c62ab45caf8116ef6
JairoPreciado <jairopreciadoayala@gmail.com>
2022-11-18 12:13:09 -0600

    examen

--- examen.c
+++ examen.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int list[0];
 int pseudo(int list[], int n){
-   int min=0;
-   int indexMin=0;
 
    for(int i=0; i<n-1; i++){
-       min = i;
+      int  min = i;
        
        for(int j=i+1; j<n; j++){
 	   if(list[j] < list[min]){
 		   min = j;
 	   }
        }
-       if(indexMin !=i){
+       if(min !=i){
 	  swap(&list[min], &list[i]);
        }
    }
 }
 
-int swap(int *x, int *y){
-   int temp = *x;
-   *x = *y;
-   *y = temp;
+int swap(int *a, int *b){
+   int temp = *a;
+   *a = *b;
+   *b = temp;
 }
 
 int main(){
